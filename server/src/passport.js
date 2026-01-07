a903d52156afb983c326f1633e9ff7047e361d3a
LG_ParkHonSeark <honspark@lgcns.com>
2026-01-07 19:51:59 +0900

    fix hard coding

--- server/src/passport.js
+++ server/src/passport.js
@@ -20,7 +20,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || '/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         // Build user object from profile
